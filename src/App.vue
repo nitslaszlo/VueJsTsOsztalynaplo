0fb30e6f4b389490bf9f6710ab808df725f74054
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-08-29 13:28:30 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- oldalcím -->
+        <h1>{{ pageTitle }}</h1>
+        <!-- diákok -->
+        <ul>
+            <li v-for="student in studentCollection" v-bind:key="student">
+                <span v-bind:style="GetColor(student)">
+                    {{ student.name }}
+                </span>
+                --
+                <span v-show="student.gender == 'lány'">
+                    Nőnapon virágot kap majd</span>
+                <span v-if="student.gender == 'fiú'">
+                    Gyereknapon labdát kap majd</span>
+            </li>
+        </ul>
+
+        <!-- Új tanuló felvétele -->
+        <input type="text" v-model="newStudent.name" v-on:keydown.enter="AddNewStudent()">
+        <br>
+        <label>
+            Lány
+            <input type="radio" name="gender" value="lány" v-model="newStudent.gender">
+        </label>
+        <label>
+            Fiú
+            <input type="radio" name="gender" value="fiú" v-model="newStudent.gender">
+        </label>
+        <br>
+        <button v-on:click="AddNewStudent()">felvétel az osztályba</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
-@Component({
-  components: {
-    HelloWorld
+class Student {
+  name: string = "";
+  gender: string = "";
+}
+
+@Component
+export default class App extends Vue {
+  private pageTitle: string = "Osztálynapló";
+  private studentStyle = {
+    color: "white",
+    backgroundColor: "gray"
+  };
+  private newStudent = new Student();
+  private studentCollection: Student[] = [
+    {
+      name: "Kata",
+      gender: "lány"
+    },
+    {
+      name: "Anna",
+      gender: "lány"
+    },
+    {
+      name: "Panka",
+      gender: "lány"
+    },
+    {
+      name: "Máté",
+      gender: "fiú"
+    },
+    {
+      name: "Tomi",
+      gender: "fiú"
+    },
+    {
+      name: "Andris",
+      gender: "fiú"
+    },
+    {
+      name: "Csaba",
+      gender: "fiú"
+    },
+    {
+      name: "Bence",
+      gender: "fiú"
+    }
+  ];
+
+  private AddNewStudent(): void {
+    if (!this.newStudent.gender || !this.newStudent.name) {
+      return;
+    }
+    this.studentCollection.push(this.newStudent);
+    this.newStudent = new Student();
   }
-})
-export default class App extends Vue {}
+
+  private GetColor(student: Student): any {
+    let backgroundColor = student.gender == "fiú" ? "blue" : "red";
+    return {
+      color: "white",
+      backgroundColor: backgroundColor
+    };
+  }
+}
 </script>
 
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
